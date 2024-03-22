540eb6bbece7cd6eace0f29dc646f6494e8b5958
robmg9655 <163440586+robmg9655@users.noreply.github.com>
2024-03-22 02:34:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,12 @@
-const formOpenBtn = document.querySelector("#form-open"),
-  home = document.querySelector(".home"),
-  formContainer = document.querySelector(".form_container"),
-  formCloseBtn = document.querySelector(".form_close"),
-  signupBtn = document.querySelector("#signup"),
-  loginBtn = document.querySelector("#login"),
-  pwShowHide = document.querySelectorAll(".pw_hide");
+// DISCORD SERVER LINK
+let openLinkInNewTab00 = () => {
+  window.open("https://discord.gg/FPZq9BpN", "_blank");
+};
+// TWITER LINK
+let openLinkInNewTab01 = () => {
+  window.open("https://twitter.com/Ejeedao", "_blank");
+};
 
-formOpenBtn.addEventListener("click", () => home.classList.add("show"));
-formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
-
-pwShowHide.forEach((icon) => {
-  icon.addEventListener("click", () => {
-    let getPwInput = icon.parentElement.querySelector("input");
-    if (getPwInput.type === "password") {
-      getPwInput.type = "text";
-      icon.classList.replace("uil-eye-slash", "uil-eye");
-    } else {
-      getPwInput.type = "password";
-      icon.classList.replace("uil-eye", "uil-eye-slash");
-    }
-  });
-});
-
-signupBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  formContainer.classList.add("active");
-});
-loginBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  formContainer.classList.remove("active");
-});
+/*let openLinkInNewTab02 = () => {
+  window.open("https://discord.gg/FPZq9BpN", "_blank");
+};*/
